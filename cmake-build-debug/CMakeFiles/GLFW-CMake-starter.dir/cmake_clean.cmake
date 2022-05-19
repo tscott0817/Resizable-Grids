9f3d0edbdaa6ef978ab06fcaa6a09971373488d2
ToastedKielbasa <tylerscott.home@gmail.com>
2022-05-19 16:21:04 -0400

    GLFW start

---
+++ cmake-build-debug/CMakeFiles/GLFW-CMake-starter.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/GLFW-CMake-starter.dir/main.cpp.obj"
+  "GLFW-CMake-starter.exe"
+  "GLFW-CMake-starter.exe.manifest"
+  "GLFW-CMake-starter.pdb"
+  "libGLFW-CMake-starter.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/GLFW-CMake-starter.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ cmake-build-debug/CMakeFiles/Grid.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/Grid.dir/main.cpp.obj"
+  "Grid.exe"
+  "Grid.exe.manifest"
+  "Grid.pdb"
+  "libGrid.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/Grid.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
